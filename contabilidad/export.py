a8cc2f17be1d100d10869673890670abe89c0ba5
Roberto Marroquin <robma96@gmail.com>
2022-03-25 13:10:54 -0600

    exists

--- contabilidad/export.py
+++ contabilidad/export.py
@@ -260,11 +260,12 @@ def imprimir_resumen_auxiliar_diario_mayor(libro):
         row+=1
         for fecha in fechas:
             movs_fecha = movs.filter(partida__libro=libro,cuenta__codigo=c,partida_fecha=fecha)
-            ws.set_row(row,20)
-            ws.write(row,1,f"{fecha.strftime('%d/%m/%Y')}",body_format)
-            ws.write(row,5,"${0:.2f}".format(movs_fecha.aggregate(total=Coalesce(Sum("monto_haber"),0))["total"]),body_format)
-            ws.write(row,6,"${0:.2f}".format(movs_fecha.aggregate(total=Coalesce(Sum("monto_daber"),0))["total"]),body_format)
-            row+=1
+            if movs_fecha.exists():    
+                ws.set_row(row,20)
+                ws.write(row,1,f"{fecha.strftime('%d/%m/%Y')}",body_format)
+                ws.write(row,5,"${0:.2f}".format(movs_fecha.aggregate(total=Coalesce(Sum("monto_haber"),0))["total"]),body_format)
+                ws.write(row,6,"${0:.2f}".format(movs_fecha.aggregate(total=Coalesce(Sum("monto_daber"),0))["total"]),body_format)
+                row+=1
         #for movimiento in movs.filter(partida__libro=libro,cuenta__codigo=c):
         #    ws.set_row(row,20)
         #    ws.write(row,1,f"{movimiento.partida.fecha.strftime('%d/%m/%Y')}",body_format)
