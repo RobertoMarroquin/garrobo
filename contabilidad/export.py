50e88df6e666f93d7413ad896a8bf19f777c94c7
Roberto Marroquin <robma96@gmail.com>
2022-03-25 13:23:53 -0600

    partida__fecha

--- contabilidad/export.py
+++ contabilidad/export.py
@@ -259,9 +259,8 @@ def imprimir_resumen_auxiliar_diario_mayor(libro):
         #Resumen de cuentas
         row+=1
         for fecha in fechas:
-            print(fecha)
             if largo != 1:
-                movs_fecha = movs.filter(partida__libro=libro,cuenta__codigo=c,partida__fecha=fecha.strftime("%Y-%m-%d"))
+                movs_fecha = movs.filter(partida__libro=libro,cuenta__codigo=c,partida__fecha=fecha['partida__fecha'].strftime("%Y-%m-%d"))
                 if movs_fecha.exists():    
                     ws.set_row(row,20)
                     ws.write(row,1,f"{fecha.strftime('%d/%m/%Y')}",body_format)
